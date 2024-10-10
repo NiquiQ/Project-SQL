52dc1a974797b3924e2c0234209f99115a19d7a2
NiquiQ <82652994+NiquiQ@users.noreply.github.com>
2024-10-10 15:17:17 +0300

    FIX

--- Scripts/api_nbu.sql
+++ Scripts/api_nbu.sql
@@ -13,7 +13,7 @@ select * from sys_params
 
 
 INSERT INTO sys_params (param_name, value_date, value_text, param_descr)
-VALUES ('list_currencies', TRUNC(SYSDATE), 'USD,EUR,KZT,AMD,GBP,ILS', 'Список валют для синхронізації в процедурі util.api_nbu_sync');
+VALUES ('list_currencies', TRUNC(SYSDATE), 'USD,EUR,KZT,AMD,GBP,ILS', 'РЎРїРёСЃРѕРє РІР°Р»СЋС‚ РґР»СЏ СЃРёРЅС…СЂРѕРЅС–Р·Р°С†С–С— РІ РїСЂРѕС†РµРґСѓСЂС– util.api_nbu_sync');
 
 select * from cur_exchange
 
